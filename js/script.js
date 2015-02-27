aa7a63dee95cce4111a1066f427daade451d0f40
xTrebliG <gil@crankshiftnyc.com>
2015-02-27 12:46:06 -0500

    Updated home page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,40 @@
+$('#downarrow').hide();
 
 $(document).ready(function(){
-    $('#fullpage').fullpage(
-        //alert('Hello World!')
-    );
+
+    $('#fullpage').fullpage(function(){
+
+    });
+
+    nameSlideRight();
+    arrowFadeIn();
 
 });
+
+function nameSlideRight(){
+    $(window).load(function(){
+        $('#one').animate({
+            opacity: 1,
+            marginLeft: '90%'
+        }, 1400);
+
+        $('#two').animate({
+            opacity: 1,
+            marginLeft: '85%'
+        }, 1500);
+
+        $('#webdev').css('margin-left', '95px')
+
+
+    })
+
+}
+
+
+function arrowFadeIn(){
+    $(window).load(function(){
+        $('#downarrow').fadeIn(8000)
+
+    })
+
+}
